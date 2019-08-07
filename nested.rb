702d510113033d93f43fc35ac088bb8b96f5f7b3
loading-actor-9896 <upalazzo93@gmail.com>
2019-08-07 16:23:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -22,7 +22,7 @@ end
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
-puts 	programmer_hash[:alan_kay][:known_for]
+
 	
 	programmer_hash = 
  		{
@@ -39,6 +39,8 @@ puts 	programmer_hash[:alan_kay][:known_for]
           :languages => ["C"]
         }
      }
+     
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
