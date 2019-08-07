853191180abe482ba8c1ece7672ffdb65e71b85b
loading-actor-9896 <upalazzo93@gmail.com>
2019-08-07 16:21:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -22,7 +22,7 @@ end
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
-	:known_for
+puts 	programmer_hash[:alan_kay][:known_for]
 	
 	programmer_hash = 
  		{
